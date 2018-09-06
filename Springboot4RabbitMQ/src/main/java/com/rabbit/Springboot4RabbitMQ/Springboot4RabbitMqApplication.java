323054fa10d58ade48ef3e4776a74009a4dc9b57
KillMonkey <1249082798@qq.com>
2018-09-06 20:47:47 +0800

    RabbitMQ项目初始化

---
+++ Springboot4RabbitMQ/src/main/java/com/rabbit/Springboot4RabbitMQ/Springboot4RabbitMqApplication.java
@@ -0,0 +1,12 @@
+package com.rabbit.Springboot4RabbitMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot4RabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot4RabbitMqApplication.class, args);
+	}
+}
